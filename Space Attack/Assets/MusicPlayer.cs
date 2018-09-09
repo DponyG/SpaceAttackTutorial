378fe56602deb05413b2a83d837dad1684e23e30
Darkpony <Darkpony>
2018-09-09 09:38:17 -0800

    Pushing Fixed Master

---
+++ Space Attack/Assets/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 2f;
+    private bool created = false;
+
+    void Awake() {
+        if (!created) {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+    }
+
+    void LoadFirstLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+
+    // Use this for initialization
+    void Start() {
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
